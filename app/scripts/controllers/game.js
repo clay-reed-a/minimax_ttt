012a42a174227bb2cbc6e0d5badacb5a70d5bc03
Clayton Albachten <clay.reed.a@gmail.com>
2014-12-20 19:15:58 -0500

    AI vs. Human! Human vs. AI! Human vs. Human

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -11,8 +11,35 @@ angular.module('tictactoeApp')
   .controller('GameCtrl', function ($scope, GameLogic, AiLogic) {
     $scope.board = null;
 
+    $scope.opponent = 'AI';
+    $scope.goesFirst = 'Me';
+    $scope.opponentMark = 'x';
+    $scope.playerMark = 'o';
+
     $scope.newGame = function() {
-      $scope.board = GameLogic.newBoard(); 
+      $scope.board = GameLogic.newBoard();
+
+      
+      $scope.playerMark = ($scope.opponentMark === 'x') ? 'o' : 'x';
+
+      if (($scope.goesFirst === 'Them')
+        && 
+          ($scope.opponent === 'human')) {
+        var cache = $scope.playerMark;
+        $scope.playerMark = $scope.opponentMark,
+        $scope.opponentMark = cache; 
+      }
+
+      if ($scope.opponent === 'AI') {
+        AiLogic.me = $scope.opponentMark;
+        AiLogic.them = $scope.playerMark;  
+      }
+      if (($scope.goesFirst === 'Them') 
+        && 
+          ($scope.opponent === 'AI')) {
+        var openingMove = AiLogic.decideMove($scope.board);
+        $scope.aiMove(openingMove);
+      }
     };
 
     $scope.aiMove = function(where) {
@@ -20,32 +47,41 @@ angular.module('tictactoeApp')
       var row = where[0],
           col = where[1];
 
-       $scope.board[row][col].space = 'o'; 
+       $scope.board[row][col].space = $scope.opponentMark; 
     };
 
     $scope.userMove = function(where) {
       var rowIndex = where.row, 
           colIndex = where.column;
       
-      $scope.board[rowIndex][colIndex].space = 'x';
+      $scope.board[rowIndex][colIndex].space = $scope.playerMark;
       if (GameLogic.won($scope.board)) {
-        console.log('x wins!');
+        console.log($scope.playerMark+' wins!');
         $scope.board = null;
       } else {
         if (GameLogic.draw($scope.board)) {
           console.log('nobody wins!');
           $scope.board = null;
         } else {
-          var response = AiLogic.decideMove($scope.board);
-          $scope.aiMove(response);
-          if (GameLogic.won($scope.board)) {
-            console.log('o wins!');
-            $scope.board = null;
-          } else {
-            if (GameLogic.draw($scope.board)) {
-              console.log('nobody wins!');
+          if ($scope.opponent === 'AI') {
+            var response = AiLogic.decideMove($scope.board);
+            $scope.aiMove(response);
+            if (GameLogic.won($scope.board)) {
+              console.log(AiLogic.me+' wins!');
               $scope.board = null;
+            } else {
+              if (GameLogic.draw($scope.board)) {
+                console.log('nobody wins!');
+                $scope.board = null;
+              }
             }
+          } else {
+
+            var cache = $scope.playerMark;
+            $scope.playerMark = $scope.opponentMark,
+            $scope.opponentMark = cache; 
+
+    
           }
         }
       }      
--- app/scripts/services/ailogic.js
+++ app/scripts/services/ailogic.js
@@ -11,49 +11,50 @@ angular.module('tictactoeApp')
   .factory('AiLogic', function () {
     var ailogic = {};
 
+    ailogic.them = 'x';
     ailogic.me = 'o'; 
 
     ailogic.randomElement = function(arr) {
       var randomIndex = Math.floor(Math.random() * arr.length);
-      console.log(randomIndex);
       return arr[randomIndex];
     };
 
     ailogic.decideMove = function(board) {
-      var me = this.me;
+      var me = this.me,
+          them = this.them;
       var iCanWinHere = this.hasTwoInARow(board, me),
-       theyCanWinHere = this.hasTwoInARow(board, 'x'), 
+       theyCanWinHere = this.hasTwoInARow(board, them), 
        iCanForkHere = this.spotFork(board, me),
-       theyCanForkHere = this.spotFork(board, 'x');
+       theyCanForkHere = this.spotFork(board, them);
       if (iCanWinHere) {
         console.log("I have won!");
-        console.log(JSON.stringify(iCanWinHere));
+
         return iCanWinHere[0];
       } else if (theyCanWinHere) {
         console.log("They are winning!");
-        console.log(JSON.stringify(theyCanWinHere));
+
         return theyCanWinHere[0]; 
       } else if (iCanForkHere) { 
         console.log("I will win!");
-        console.log(JSON.stringify(iCanForkHere));
-        return iCanForkHere[0];
+
+        return this.randomElement(iCanForkHere);
       } else if (theyCanForkHere) {
         console.log("They might fork!");
 
         if (theyCanForkHere.length === 2) {
           console.log("I will get them!");
           var attacks = this.findAgress(board);
-          console.log(JSON.stringify(attacks));
+
           return this.randomElement(attacks);
         } else {
           console.log("I will block them!");
-          console.log(JSON.stringify(theyCanForkHere));
+
           return theyCanForkHere[0];
         }
       } else {
         console.log("I should move here!");
         var best_moves = this.chooseLegalMove(board); 
-        console.log(JSON.stringify(best_moves));
+
         return this.randomElement(best_moves);
       }
     };
@@ -146,8 +147,8 @@ angular.module('tictactoeApp')
           for (var b = 0; b < theyBlock.length; b++) {
             var block = theyBlock[b];
     
-            imaginaryBoard[block[0]][block[1]].space = 'x';
-            var forcedIntoFork = this.haveFork(imaginaryBoard, 'x');
+            imaginaryBoard[block[0]][block[1]].space = this.them;
+            var forcedIntoFork = this.haveFork(imaginaryBoard, this.them);
             var notForcedIntoFork = !forcedIntoFork;
             if (notForcedIntoFork) {
               attacks.push([emptySpace.row, emptySpace.column]);

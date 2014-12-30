17c6a099477e7399b1750c96259c93f70ab44be2
Clayton Albachten <clay.reed.a@gmail.com>
2014-12-29 22:00:12 -0500

    "Maximum call stack size exceeded", yes!

--- app/scripts/services/ailogic.js
+++ app/scripts/services/ailogic.js
@@ -14,212 +14,123 @@ angular.module('tictactoeApp')
     ailogic.them = 'x';
     ailogic.me = 'o'; 
 
-    ailogic.randomElement = function(arr) {
-      var randomIndex = Math.floor(Math.random() * arr.length);
-      return arr[randomIndex];
+    
+    ailogic.max = function(arr) {
+      max_el = Math.max.apply(null, arr);
+      return arr.indexOf(max_el);
+    };
+
+    ailogic.min = function(arr) {
+      min_el = Math.min.apply(null, arr);
+      return arr.indexOf(min_el);
     };
 
     ailogic.decideMove = function(board) {
-      var me = this.me,
-          them = this.them;
-      var iCanWinHere = this.hasTwoInARow(board, me),
-       theyCanWinHere = this.hasTwoInARow(board, them), 
-       iCanForkHere = this.spotFork(board, me),
-       theyCanForkHere = this.spotFork(board, them);
-      if (iCanWinHere) {
-        console.log('I win again!');
+      var me = this.me;
+      return this.minimax(board, me)[1];
+    };
 
-        return iCanWinHere[0];
-      } else if (theyCanWinHere) {
-        console.log('They can\'t win!');
+    ailogic.minimax = function(board, turnPlayer) {
 
-        return theyCanWinHere[0]; 
-      } else if (iCanForkHere) { 
-        console.log('I will win!');
+      var wonGame = this.won(board); 
+      var availableMoves = this.getAvailableMoves(board); 
+      var noMoves = availableMoves.length === 0; 
+      var endGameConditions = wonGame || noMoves; 
+      var playerFlip = this.flipPlayer(turnPlayer);
+      var scores = [];
+      var moves = [];
 
-        return this.randomElement(iCanForkHere);
-      } else if (theyCanForkHere) {
-        console.log('They might fork!');
- 
-        if (theyCanForkHere.length > 1) {
-          console.log('I will get them!');
-          var attacks = this.findAgress(board);
-          console.log(JSON.stringify(attacks)); 
-          return this.randomElement(attacks);
+      if (endGameConditions) {
+        return [this.score(wonGame)]; 
+      }
+
+      for (var a = 0; a < availableMoves.length; a++) {
+        var move = availableMoves[a]; 
+        var imaginaryBoard = angular.copy(board); 
+        var evaluation = this.minimax(board, playerFlip);
+        scores.push(evaluation[0]);
+        moves.push(evaluation[1]);
+      }
+
+      if (turnPlayer === this.me) {
+        var max_idx = this.max(scores);
+        var max = scores[max_idx]; 
+        var choice = moves[max_idx]; 
+        return [max, choice]; 
+      } else {
+        var min_idx = this.min(scores);
+        var min = scores[min_idx];
+        var choice = moves[min_idx];
+        return [min, choice];
+      }
+    };
+
+    ailogic.flipPlayer = function(player) {
+      if (player === 'x')
+        return 'o'; 
+      if (player === 'o')
+        return 'x'; 
+    };
+
+    ailogic.score = function(wonGame, currentPlayer) {
+      if (wonGame) {
+        if (wonGame == currentPlayer) {
+          return Number.POSITIVE_INFINITY; 
         } else {
-          console.log('I will stop them!');
-
-          return theyCanForkHere[0];
+          return Number.NEGATIVE_INFINITY; 
         }
       } else {
-        console.log('I\'m gonna go here!');
-        var bestMoves = this.chooseLegalMove(board); 
-
-        return this.randomElement(bestMoves);
+        return 0;  
       }
     };
 
-    ailogic.chooseLegalMove = function(board) {
-      var moveTypes = this.legalMoves(), 
-          movesAvailable,  
-          moveType;
-      for (var t = 0; t < moveTypes.length; t++) {
-        moveType = moveTypes[t];
-        movesAvailable = this.getAvailableMoves(moveType, board);
-    
-        if (movesAvailable.length !== 0) {
-          return movesAvailable;
-        }
-      }
-    };
-
-    ailogic.getAvailableMoves = function(moveSet, board) {
-      return moveSet.filter(function(move) {
-        return board[move[0]][move[1]].space === '';  
-      });
-    };
-
-    ailogic.legalMoves = function() {
-      // legal moves are ordered  
-      // from most to least desirable: 
-      // 5 > 1 >= 3 >= 9 >= 7 > 2 >= 6 >= 8 >= 4  
-      return [
-        // center 
-        [[1, 1]], // 5 in Knuth 
-        // corners 
-        [[0, 0], // 1 in Knuth 
-         [0, 2], // 3 in Knuth 
-         [2, 2], // 9 in Knuth 
-         [2, 0]], // 7 in Knuth 
-        // sides 
-        [[0, 1], // 2 in Knuth 
-         [1, 2], // 6 in Knuth 
-         [2, 1], // 8 in Knuth 
-         [1, 0]] // 4 in Knuth  
-      ];
-    };
-
-    ailogic.twoInRow = function(cells, player) {
-      var occupied = this.count(cells, player), 
-             empty = this.count(cells, '');
-      var twoOccupied = occupied === 2,
-             oneEmpty = empty === 1;
-
-      return (twoOccupied && oneEmpty);
-    };
-
-    ailogic.count = function(arr, countElement) {
-      var countElements = arr.filter(function(arrElement) {
-        return arrElement === countElement;
-      });
-      return countElements.length; 
-    };
-
-    ailogic.considerEmptySpaces = function(board) {
-      var emptySpaces = [], 
-          row, 
-          cell;
+    ailogic.getAvailableMoves = function(board) {
+      var availableMoves = [];
       for (var r = 0; r < board.length; r++) {
-        row = board[r];
-        for(var c = 0; c < row.length; c++) {
-          cell = row[c];
-          if (cell.space === '') {
-            emptySpaces.push(cell.position);
-          }
+        var row = board[r];
+        for (var c = 0; c < row.length; c++) {
+          var cell = row[c];
+          var cellIsEmpty = cell.space === '';  
+          if (cellIsEmpty) {
+            availableMoves.push(cell.position);
+          } 
         }
       }
-      return emptySpaces;
+      return availableMoves;
     };
 
-    ailogic.findAgress = function(board) {
-      var emptySpaces = this.considerEmptySpaces(board);
-      var attacks = [];
 
-      for (var e = 0; e < emptySpaces.length; e++) {
-        var emptySpace = emptySpaces[e];
-        var imaginaryBoard = angular.copy(board);
-        imaginaryBoard[emptySpace.row][emptySpace.column].space = this.me;
-        var theyBlock = this.hasTwoInARow(imaginaryBoard, this.me); 
-    
-        if (theyBlock) {
-         
-          for (var b = 0; b < theyBlock.length; b++) {
-            var block = theyBlock[b];
-    
-            imaginaryBoard[block[0]][block[1]].space = this.them;
-            var forcedIntoFork = this.haveFork(imaginaryBoard, this.them);
-            var notForcedIntoFork = !forcedIntoFork;
-            if (notForcedIntoFork) {
-              attacks.push([emptySpace.row, emptySpace.column]);
-            } 
-          }
 
-        }
-      }
-
-      return attacks; 
-    };
-
-    ailogic.hasTwoInARow = function(board, player) {
-      var wins = this.allWins(),
-      movesToWin = [];
-      for (var i = 0; i < wins.length; i++) {
+    ailogic.won = function(board) {
+      var wins = this.allWins();
+      for(var i = 0; i < wins.length; i++) {  
         var win = wins[i];
 
-        var cells = this.getCells(win, board);
-
-        if (this.twoInRow(cells, player)) {
-          var index = cells.indexOf(''),
-               move = [win[index].row, win[index].column];
-
-          movesToWin.push(move);
+        var cells = this.getCells(board, win);
+       
+        if (this.threeInRow(cells)) {
+          return cells[0];
         }
       }
-      if (movesToWin.length === 0) {
-        return false;  
-      } else {
-        return movesToWin;
-      }
+
+      return false; 
     };
 
-    ailogic.getCells = function(row, board) {
-      return row.map(function(cell) {
+    ailogic.getCells = function(board, win) {
+      return win.map(function(cell) {
         return board[cell.row][cell.column].space; 
       });
     };
 
-    ailogic.spotFork = function(board, player) {
-      var emptySpaces = this.considerEmptySpaces(board),
-          emptySpace, 
-          imaginaryBoard, 
-                emptyRow,
-                emptyCol, 
-                    wins, 
-                   forks = [];
-      for (var e = 0; e < emptySpaces.length; e++) {
-        emptySpace = emptySpaces[e]; 
-        imaginaryBoard = angular.copy(board);
-
-        emptyRow = emptySpace.row; 
-        emptyCol = emptySpace.column;
-
-        imaginaryBoard[emptyRow][emptyCol].space = player;
-        wins = this.hasTwoInARow(imaginaryBoard, player);
-        if (wins.length === 2) {
-          forks.push([emptyRow, emptyCol]); 
-        }
-      }
-      if (forks.length !== 0) {
-
-        return forks;
-      } else {
-        return false; 
-      }
-    };
-
-    ailogic.haveFork = function(board, player) {
-      var sets = this.hasTwoInARow(board, player);
-      return sets.length === 2; 
+    ailogic.threeInRow = function(cells) {
+      var firstCell = cells[0], 
+         secondCell = cells[1], 
+          thirdCell = cells[2];
+      return (
+        (firstCell === secondCell) && 
+        (secondCell === thirdCell) &&
+        (firstCell !== '')
+      );
     };
 
     ailogic.allWins = function() {
